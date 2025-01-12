18f0395a21d51097aa2ec3590f85748cfcd800a2
Isay Razilov <isay@yukidata.com>
2025-01-12 16:46:06 +0200

    Adding load balancer name

--- modules/yuki-proxy/modules/ingress/main.tf
+++ modules/yuki-proxy/modules/ingress/main.tf
@@ -1,5 +1,5 @@
 resource "kubernetes_ingress_v1" "ingress" {
-  count = var.create_private_load_balancers ? 1 : 0
+  # count = var.create_private_load_balancers ? 1 : 0
   metadata {
     name        = var.ingress_name
     namespace   = var.namespace
