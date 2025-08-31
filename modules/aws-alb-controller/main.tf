7327427be1dbcded19a3b0933e4eab4dad6970cb
yuki-tal <tal@yukidata.com>
2025-08-31 14:18:38 +0300

    constrain lb module version (#16)

--- modules/aws-alb-controller/main.tf
+++ modules/aws-alb-controller/main.tf
@@ -1,5 +1,6 @@
 module "lb_role" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.0"
 
   role_name                              = "${var.cluster_name}-${var.env_name}_eks_lb"
   attach_load_balancer_controller_policy = true
